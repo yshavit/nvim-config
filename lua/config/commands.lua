4a0780e3c5fad57b8f6c7f370fb28d636a3fc544
Yuval Shavit <dev@yuvalshavit.com>
2025-11-13 11:28:47 -0500

    improve Scratch and Memdiff functions

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,26 +1,39 @@
+-- Format text inline
 vim.api.nvim_create_user_command("FormatJson", function(opts)
   local cmd = string.format("%d,%d!jq .", opts.line1, opts.line2)
   vim.cmd(cmd)
 end, { range = true, desc = "Format JSON in range" })
 
-vim.api.nvim_create_user_command("Scratch", function()
+-- Scratch buffers
+_G.scratch_counter = _G.scratch_counter or 0
+vim.api.nvim_create_user_command("Scratch", function(opts)
   vim.cmd("enew")
-  vim.cmd("setlocal buftype=nofile")
-  vim.cmd("setlocal bufhidden=wipe")
+  _G.scratch_counter = _G.scratch_counter + 1
+  local prefix = opts.args ~= "" and opts.args .. " " or ""
+  local name = prefix .. "Scratch " .. _G.scratch_counter
+  vim.api.nvim_buf_set_name(0, name)
+  vim.cmd("setlocal buftype=nowrite")
+  vim.cmd("setlocal bufhidden=hide")
   vim.cmd("setlocal noswapfile")
-end, { desc = "Create a new scratch buffer" })
+end, { nargs = "?", desc = "Create a new scratch buffer" })
 
-vim.api.nvim_create_user_command("VScratch", function()
+vim.api.nvim_create_user_command("VScratch", function(opts)
   vim.cmd("vnew")
-  vim.cmd("setlocal buftype=nofile")
-  vim.cmd("setlocal bufhidden=wipe")
+  _G.scratch_counter = _G.scratch_counter or 0
+  _G.scratch_counter = _G.scratch_counter + 1
+  local prefix = opts.args ~= "" and opts.args .. " " or ""
+  local name = prefix .. "Scratch " .. _G.scratch_counter
+  vim.api.nvim_buf_set_name(0, name)
+  vim.cmd("setlocal buftype=nowrite")
+  vim.cmd("setlocal bufhidden=hide")
   vim.cmd("setlocal noswapfile")
-end, { desc = "Create a new scratch buffer in a vertical split" })
+end, { nargs = "?", desc = "Create a new scratch buffer in vertical split" })
 
+-- Memdiff
 vim.api.nvim_create_user_command("Memdiff", function()
-  vim.cmd("Scratch")
+  vim.cmd("Scratch Diff")
   vim.cmd("diffthis")
-  vim.cmd("VScratch")
+  vim.cmd("VScratch Diff")
   vim.cmd("diffthis")
   vim.cmd("wincmd h") -- Move cursor to left window
 end, { desc = "Diff current buffer against a new scratch buffer" })
