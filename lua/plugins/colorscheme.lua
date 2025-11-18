09f293a4bba1625f6517252d294cfc3f2ea97f34
Yuval Shavit <dev@yuvalshavit.com>
2025-11-18 11:16:36 -0500

    nicer relnums

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
     opts = {
       on_highlights = function(hl, c)
         hl.WinSeparator = { fg = "#4a148c", bg = c.bg }
+        hl.LineNrBelow = { fg = "#684d8a" } -- uses tokyonight's gutter foreground
+        hl.LineNrAbove = { fg = "#684d8a" }
+        hl.CursorLineNr = { fg = "#b24cd4", bold = true }
       end,
     },
   },
