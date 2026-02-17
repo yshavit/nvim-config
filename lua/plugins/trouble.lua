a420adde8ba3c06599a529a7e90ac4a1a84e95ab
Yuval Shavit <dev@yuvalshavit.com>
2026-02-16 20:11:58 -0500

    trouble: auto-close when losing focus

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -32,4 +32,20 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("trouble").setup(opts)
+
+    -- Auto-close Trouble symbols when focus is lost
+    vim.api.nvim_create_autocmd("BufLeave", {
+      pattern = "*",
+      callback = function()
+        local trouble = require("trouble")
+        -- Check if we're leaving a Trouble buffer
+        if vim.bo.filetype == "trouble" then
+          -- Close the symbols mode specifically
+          trouble.close("symbols")
+        end
+      end,
+    })
+  end,
 }
