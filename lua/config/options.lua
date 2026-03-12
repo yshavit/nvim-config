10e915ba65f9e51fe5b22db0720c188ffeb9d211
Yuval Shavit <dev@yuvalshavit.com>
2026-03-12 00:44:53 -0400

    clipboard: fix perf for windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,38 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.clipboard = "unnamedplus"
 vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add,./spell/local.utf-8.add"
 vim.o.showcmd = false
+
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has("wsl") == 1 then
+  -- Don't use unnamedplus. Instead, use native nvim buffers, and then sync
+  -- to and from the windows clipboard on nvim [un]focus.
+  vim.opt.clipboard = ""
+
+  vim.api.nvim_create_autocmd({ "FocusGained", "VimEnter" }, {
+    callback = function()
+      local sys = vim.fn.getreg("+")
+      if sys ~= "" then
+        vim.fn.setreg('"', sys)
+      end
+    end,
+  })
+
+  local did_yank = false
+
+  vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+      did_yank = true
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("FocusLost", {
+    callback = function()
+      if did_yank then
+        vim.fn.setreg("+", vim.fn.getreg('"'))
+        did_yank = false
+      end
+    end,
+  })
+end
