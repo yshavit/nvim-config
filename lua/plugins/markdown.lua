69b70b2d45f4f38e4c468574b0a0417b375dbeb2
Yuval Shavit <dev@yuvalshavit.com>
2026-04-07 15:43:34 -0400

    markdown: default lint ignores

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = { "--config", vim.fn.expand("~/.config/nvim/markdownlint.json") },
+        },
+      },
+    },
+  },
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       latex = {
