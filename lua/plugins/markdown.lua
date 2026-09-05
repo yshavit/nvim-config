3831833e5bf5fe1ced59d11e87f55df4b22a10fb
Yuval Shavit <dev@yuvalshavit.com>
2026-09-04 21:45:08 -0400

    markdown: add table wrapping
    <leader>tf (Table Float)

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -11,6 +11,28 @@ return {
     },
   },
   {
+    -- Won't be needed after https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/616 is implemented
+    "ice345/markdown-table-wrap.nvim",
+    -- Codex reviewed this commit for safety on 2026-09-04; review again before updating.
+    commit = "281069c1107a510028fab5f5b0a74a226ed2036d",
+    ft = { "markdown", "quarto", "rmd" },
+    opts = {
+      -- Keep normal reading/editing in Source; use wrapped tables only on demand.
+      auto_preview = false,
+      max_col_width = 120,
+      max_width_ratio = 0.95,
+      fit_to_window = true,
+    },
+    keys = {
+      {
+        "<leader>tf",
+        "<cmd>MarkdownTableFloatPreview<cr>",
+        desc = "Table Float",
+        ft = { "markdown", "quarto", "rmd" },
+      },
+    },
+  },
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       latex = {
